c88c87b76fd6b26fb6c98143a7dcef5e990a77e1
Sai Sumiran <164289250+Sumiran2105@users.noreply.github.com>
2026-04-02 10:37:15 +0530

    Passwords updated

--- src/components/auth/Login/Login.jsx
+++ src/components/auth/Login/Login.jsx
@@ -1,6 +1,6 @@
 const demoUsers = [
   { email: 'admin@dcm.demo', password: 'admin123', role: 'ADMIN' },
-  { email: 'doctor@dcm.demo', password: 'doc123', role: 'DOCTOR' },
+  { email: 'doctor@dcm.demo', password: 'doc@1234', role: 'DOCTOR' },
   { email: 'nurse@dcm.demo', password: 'nurse123', role: 'NURSE' },
   { email: 'reception@dcm.demo', password: 'reception123', role: 'RECEPTIONIST' },
   { email: 'super@dcm.demo', password: 'sup123', role: 'SUPER_ADMIN' },
